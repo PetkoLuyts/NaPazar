be2fb359ffec70c4e173518907cfcf908a129246
PetkoLyuts <simona2000petko@abv.bg>
2024-02-11 17:27:36 +0200

    Add microservice for scraping data

---
+++ Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/ScrapeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.scrapeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrapeServiceApplication.class, args);
+    }
+
+}
