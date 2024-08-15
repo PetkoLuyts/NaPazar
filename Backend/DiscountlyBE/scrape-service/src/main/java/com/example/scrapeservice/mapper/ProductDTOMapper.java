fe65ec495a60e912c47d63c21fdfc1f27d0be14a
PetkoLyuts <simona2000petko@abv.bg>
2024-08-15 10:21:22 +0300

    feat: implement mapper

---
+++ Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/mapper/ProductDTOMapper.java
@@ -0,0 +1,20 @@
+package com.example.scrapeservice.mapper;
+
+import com.example.scrapeservice.dto.ProductDTO;
+import com.example.scrapeservice.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class ProductDTOMapper implements Function<Product, ProductDTO> {
+    @Override
+    public ProductDTO apply(Product product) {
+        return new ProductDTO(
+                product.getTitle(),
+                product.getOldPrice(),
+                product.getNewPrice(),
+                product.getDiscountPhrase()
+        );
+    }
+}
