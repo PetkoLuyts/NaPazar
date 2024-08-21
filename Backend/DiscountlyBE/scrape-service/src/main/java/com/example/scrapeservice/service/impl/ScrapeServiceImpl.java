5c8aadfa08bded409e67d4049e01e75ba1220d9c
PetkoLyuts <simona2000petko@abv.bg>
2024-08-21 23:39:46 +0300

    feat: add lidl product extraction

--- Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/service/impl/ScrapeServiceImpl.java
+++ Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/service/impl/ScrapeServiceImpl.java
@@ -9,6 +9,8 @@ import com.example.scrapeservice.service.PromotionService;
 import com.example.scrapeservice.service.ScrapeService;
 import com.example.scrapeservice.service.StoreService;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -189,7 +191,52 @@ public class ScrapeServiceImpl implements ScrapeService {
     public void scrapeLidlData() {
         List<String> lidlCategories = getLidlCategoriesUrls();
 
+        for(String lidlCategory : lidlCategories) {
+            List<String> productUrls = getLidlCategoryProductsUrl(lidlCategory);
+
+            for(String productUrl : productUrls) {
+
+
+            }
+        }
+
         System.out.println("test");
+    }
+
+    public List<String> getLidlCategoryProductsUrl(String categoryUrl) {
+        List<String> categoryProductsUrls = new ArrayList<>();
+
+        try {
+            Document doc = Jsoup.connect(categoryUrl).get();
+
+            if (doc != null) {
+                Elements productDivs = doc.select("div.AProductGridbox__GridTilePlaceholder");
+
+                for (Element productDiv : productDivs) {
+                    String jsonData = productDiv.attr("data-grid-data");
+                    JSONArray jsonArray = new JSONArray(jsonData);
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject productData = jsonArray.getJSONObject(i);
+
+                        String title = productData.optJSONObject("keyfacts").optString("fullTitle", "N/A");
+                        String discountPhrase = productData.optJSONObject("price").optJSONObject("discount").optString("discountText", "N/A");
+                        double oldPrice = productData.optJSONObject("price").optDouble("oldPrice", 0.0);
+                        double newPrice = productData.optJSONObject("price").optDouble("price", 0.0);
+                        String quantity = productData.optJSONObject("price").optJSONObject("basePrice").optString("text", "N/A");
+                        String description = productData.optJSONObject("keyfacts").optString("description", "N/A");
+                        
+                        String promotionText = productDiv.select(".ribbon__text").text();
+                        String promotionStarts = extractPromotionStartDate(promotionText);
+                        String promotionEnds = extractPromotionEndDate(promotionText);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return categoryProductsUrls;
     }
 
     public List<String> getLidlCategoriesUrls() {
@@ -226,5 +273,25 @@ public class ScrapeServiceImpl implements ScrapeService {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private String extractPromotionStartDate(String promotionText) {
+        if (promotionText.contains("от")) {
+            String[] parts = promotionText.split("от");
+            if (parts.length > 1) {
+                return parts[1].trim();
+            }
+        }
+        return "N/A";
+    }
+
+    private String extractPromotionEndDate(String promotionText) {
+        if (promotionText.contains("-")) {
+            String[] parts = promotionText.split("-");
+            if (parts.length > 1) {
+                return parts[1].trim();
+            }
+        }
+        return "N/A";
     }
 }
