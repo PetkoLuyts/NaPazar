04865f13483d23e1263915a8076a75e243cb2565
PetkoLyuts <simona2000petko@abv.bg>
2024-08-28 13:48:58 +0300

    feat: implement payment intent method

--- Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/service/PaymentService.java
+++ Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/service/PaymentService.java
@@ -1,4 +1,10 @@
 package com.example.scrapeservice.service;
 
+import com.example.scrapeservice.dto.PaymentInfoDTO;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+
 public interface PaymentService {
+
+    PaymentIntent createPaymentIntent(PaymentInfoDTO paymentInfoDTO) throws StripeException;
 }
--- Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/service/impl/PaymentServiceImpl.java
+++ Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/service/impl/PaymentServiceImpl.java
@@ -1,11 +1,19 @@
 package com.example.scrapeservice.service.impl;
 
+import com.example.scrapeservice.dto.PaymentInfoDTO;
 import com.example.scrapeservice.repository.PaymentRepository;
 import com.example.scrapeservice.service.PaymentService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional
@@ -16,4 +24,16 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Value("${stripe.key.secret}")
     private String stripeKey;
+
+    public PaymentIntent createPaymentIntent(PaymentInfoDTO paymentInfoDTO) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfoDTO.amount());
+        params.put("currency", paymentInfoDTO.currency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
+    }
 }
