4cac2df08ceabefd3d7850b4be4956f23a8f1e21
PetkoLyuts <simona2000petko@abv.bg>
2024-08-28 23:03:10 +0300

    feat: add app user migration

---
+++ Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/model/AppUser.java
@@ -0,0 +1,82 @@
+package com.example.scrapeservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "app_user")
+public class AppUser extends Audit implements UserDetails {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Builder.Default
+    @Column(name = "blacklisted")
+    private Boolean blacklisted = false;
+
+    @Builder.Default
+    @Column(name = "active")
+    private Boolean active = true;
+
+    @Transient
+    private List<GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ Backend/DiscountlyBE/scrape-service/src/main/resources/db/migration/V6__create_app_user_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE app_user
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100),
+    name VARCHAR(100),
+    surname VARCHAR(100),
+    blacklisted BIT,
+    active BIT,
+    created_by INT,
+    updated_by INT,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME
+);
