6a7c5d2e92ebc4b1481508eb3f9aba052eef233c
PetkoLyuts <simona2000petko@abv.bg>
2024-08-29 10:56:52 +0300

    feat: add jwt service

--- Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/dto/AuthenticationResponse.java
+++ Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/dto/AuthenticationResponse.java
@@ -1,7 +1,9 @@
 package com.example.scrapeservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 
+@Builder
 public record AuthenticationResponse(
         @JsonProperty("access_token") String accessToken,
         @JsonProperty("refresh_token") String refreshToken) {
