61d92c1e0e57e152c118ba829d139c9ae582d982
PetkoLyuts <simona2000petko@abv.bg>
2024-08-29 17:42:22 +0300

    refactor: navbar css

--- Frontend/DiscountlyFE/src/components/navbar/Navbar.tsx
+++ Frontend/DiscountlyFE/src/components/navbar/Navbar.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
+// ICONS
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <Link to="/">Home</Link>
+        <Link style={{ textDecoration: "none" }} to="/">
+          NaPazar
+        </Link>
       </div>
       <div className="navbar-cart">
-        <Link to="/checkout">Cart</Link>
+        <Link to="/checkout">
+          <ShoppingBasketIcon fontSize="large" />
+        </Link>
       </div>
     </nav>
   );
