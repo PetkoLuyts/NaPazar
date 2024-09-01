bca68d5a4d23b25264c714ce204b1351f713ff32
PetkoLyuts <simona2000petko@abv.bg>
2024-09-02 00:13:20 +0300

    feat: add find by user method in cart repo

--- Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/repository/CartRepository.java
+++ Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/repository/CartRepository.java
@@ -1,7 +1,12 @@
 package com.example.scrapeservice.repository;
 
+import com.example.scrapeservice.model.AppUser;
 import com.example.scrapeservice.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Integer> {
+
+    Optional<Cart> findByUser(AppUser user);
 }
