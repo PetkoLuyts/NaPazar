f49c7a8359d10eb2adbab401c1dd603b16e22220
PetkoLyuts <simona2000petko@abv.bg>
2024-09-01 20:05:29 +0300

    feat: add cart item model

---
+++ Backend/DiscountlyBE/scrape-service/src/main/java/com/example/scrapeservice/model/CartItem.java
@@ -0,0 +1,33 @@
+package com.example.scrapeservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cart_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @Column(name = "product_id", nullable = false)
+    private Integer productId;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+}
