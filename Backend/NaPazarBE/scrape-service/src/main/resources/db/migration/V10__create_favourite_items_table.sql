4226e51a348b854d6a3cc0b7cbbffea93c31e743
PetkoLyuts <simona2000petko@abv.bg>
2024-12-03 20:34:30 +0200

    feat: add favourite items migration

---
+++ Backend/NaPazarBE/scrape-service/src/main/resources/db/migration/V10__create_favourite_items_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE favourite_items
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    product_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES app_user(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
